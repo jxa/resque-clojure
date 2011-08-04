232452545d5e662dab3ce41e64c27af1e985f087
John Andrews + Andrew Grieser <pairing+ja+ag@edgecase.com>
2011-08-04 16:42:46 -0400

    flesh out the specs for redis.clj

--- src/resque_clojure/redis.clj
+++ src/resque_clojure/redis.clj
@@ -39,17 +39,17 @@
 
 (defn lindex [key index]
   (with-connection
-    (.lindex redis key index)))
+    (.lindex redis key (long index))))
 
 ;; returns a ArrayList. make it a seq
 (defn lrange [key start end]
   (with-connection
-    (.lrange redis key (long start) (long end))))
+    (seq (.lrange redis key (long start) (long end)))))
 
 ;; returns a HashSet. make it a seq
 (defn smembers [key]
   (with-connection
-    (.smembers redis key)))
+    (seq (.smembers redis key))))
 
 (defn sadd [key value]
   (with-connection
@@ -59,6 +59,13 @@
   (with-connection
     (.srem redis key value)))
 
+;; we could extend this to take multiple keys
 (defn del [key]
   (with-connection
-    (.del redis key)))
+    (let [args (make-array java.lang.String 1)]
+      (aset args 0 key)
+      (.del redis args))))
+
+(defn inspect []
+  (with-connection
+    redis))
--- test/resque_clojure/test/redis.clj
+++ test/resque_clojure/test/redis.clj
@@ -2,12 +2,60 @@
   (:use [resque-clojure.redis]
         [clojure.test]))
 
+(def test-key "resque-clojure-test")
+
 (use-fixtures :once
               (fn [do-tests]
                 (init {:host "localhost" :port 6379})
                 (do-tests)
                 (finalize)))
+(use-fixtures :each
+              (fn [do-tests]
+                nil
+                (do-tests)
+                (del test-key)))
 
 (deftest it-can-connect
   (is (not (nil? *pool*))))
 
+(deftest list-ops-rpush-and-lpop
+  (rpush test-key "value")
+  (is (= "value" (lpop test-key)))
+  (is (= nil (lpop test-key))))
+  
+(deftest list-ops-llen
+  (is (= 0 (llen test-key)))
+  (rpush test-key "value")
+  (is (= 1 (llen test-key))))
+
+(deftest list-ops-lindex
+  (is (nil? (lindex test-key 0)))
+  (rpush test-key "value0")
+  (rpush test-key "value1")
+  (is (= "value0" (lindex test-key 0)))
+  (is (= "value1" (lindex test-key 1))))
+
+(deftest list-ops-lrange
+  (is (empty? (lrange test-key 0 3)))
+  (rpush test-key "value0")
+  (rpush test-key "value1")
+  (is (= '("value0") (lrange test-key 0 0)))
+  (is (= '("value0" "value1") (lrange test-key 0 1))))
+
+(deftest set-ops-sadd
+  (is (empty? (smembers test-key)))
+  (sadd test-key "element0")
+  (is ( not (empty? (smembers test-key)))))
+
+(deftest set-ops-rem
+  (sadd test-key "element0")
+  (is ( not (empty? (smembers test-key))))
+  (srem test-key "element0")
+  (is (empty? (smembers test-key))))
+
+(deftest set-ops-smembers
+  (sadd test-key "element0")
+  (sadd test-key "element1")
+  (is (some #{"element0"} (smembers test-key)))
+  (is (some #{"element1"} (smembers test-key)))
+  (is (nil? (some #{"element2"} (smembers test-key)))))
