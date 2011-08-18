161cc75988423b46c876c14c777956865350269f
John Andrews + Andrew Grieser <pairing+ja+ag@edgecase.com>
2011-08-18 17:52:49 -0400

    prep for release

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject resque-clojure "0.1.0"
   :description "Redis based library for asynchronous processing"
+  :url "https://github.com/jxa/resque-clojure"
   :dependencies [[redis.clients/jedis "2.0.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[org.clojure/clojure "1.2.1"]])
+                 [org.clojure/clojure-contrib "1.2.0"]])
