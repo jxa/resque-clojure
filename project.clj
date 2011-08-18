a74cb061cedc41e406835cad7334a389ca825c89
John Andrews + Andrew Grieser <pairing+ja+ag@edgecase.com>
2011-08-18 17:28:49 -0400

    add project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject resque-clojure "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject resque-clojure "0.1.0"
+  :description "Redis based library for asynchronous processing"
   :dependencies [[redis.clients/jedis "2.0.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[org.clojure/clojure "1.2.1"]])
