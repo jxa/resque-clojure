0c9908a4d1148239fe92b8bd57e69a4b9a0b3947
John Andrews <john@edgecase.com>
2011-12-07 21:10:09 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject resque-clojure "0.2.0-SNAPSHOT"
+(defproject resque-clojure "0.2.0"
   :description "Redis based library for asynchronous processing"
   :url "https://github.com/jxa/resque-clojure"
-:dependencies [[org.clojure/clojure "1.3.0"]
-               [org.clojure/data.json "0.1.2"]
-               [redis.clients/jedis "1.5.2"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/data.json "0.1.2"]
+                 [redis.clients/jedis "1.5.2"]])
