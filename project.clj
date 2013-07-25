3cff1dd0809a0d4e038c81803c265f4682d8ebbd
John Andrews <john.m.andrews@gmail.com>
2013-07-24 20:56:50 -0400

    Prep for next version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject resque-clojure "0.2.2"
+(defproject resque-clojure "0.3.0-SNAPSHOT"
   :description "Redis based library for asynchronous processing"
   :url "https://github.com/jxa/resque-clojure"
   :dependencies [[org.clojure/data.json "0.1.2"]
